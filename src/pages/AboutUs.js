05bb7bbe8bf050b42eac5eae8d2f40aa2bacff8f
Tameem <tameem_r@hotmail.com>
2021-04-19 11:46:54 +0300

    ServicesSection component / FaqSection component

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,17 @@
 import React from "react";
 // Page components
 import AboutSection from "../components/AboutSection";
+import FaqSection from "../components/FaqSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+      <FaqSection />
+    </>
+  );
 };
 
 export default AboutUs;
