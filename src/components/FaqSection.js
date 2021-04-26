7e0efd795b26ab3c704dc851b0c3c9d56a780bd1
Tameem <tameem_r@hotmail.com>
2021-04-26 11:38:02 +0300

    Finishing FaqSection Animation

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { About } from "../styles";
 import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
 
 const FaqSection = () => {
   return (
@@ -9,9 +10,8 @@ const FaqSection = () => {
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <Toggle>
-        <div className="question">
-          <h4>How Do I Start?</h4>
+      <AnimateSharedLayout>
+        <Toggle title="How Do I Start?">
           <div className="answer">
             <p>Lorem ipsum dolor sit amet.</p>
             <p>
@@ -19,12 +19,8 @@ const FaqSection = () => {
               Necessitatibus, neque.
             </p>
           </div>
-          <div className="faq-line"></div>
-        </div>
-      </Toggle>
-      <Toggle>
-        <div className="question">
-          <h4>Daily Schedule</h4>
+        </Toggle>
+        <Toggle title="Daily Schedule">
           <div className="answer">
             <p>Lorem ipsum dolor sit amet.</p>
             <p>
@@ -32,12 +28,8 @@ const FaqSection = () => {
               Necessitatibus, neque.
             </p>
           </div>
-          <div className="faq-line"></div>
-        </div>
-      </Toggle>
-      <Toggle>
-        <div className="question">
-          <h4>Diferrent Payment Methods</h4>
+        </Toggle>
+        <Toggle title="Diferrent Payment Methods">
           <div className="answer">
             <p>Lorem ipsum dolor sit amet.</p>
             <p>
@@ -45,12 +37,8 @@ const FaqSection = () => {
               Necessitatibus, neque.
             </p>
           </div>
-          <div className="faq-line"></div>
-        </div>
-      </Toggle>
-      <Toggle>
-        <div className="question">
-          <h4>What Products do you offer.</h4>
+        </Toggle>
+        <Toggle title="What Products do you offer.">
           <div className="answer">
             <p>Lorem ipsum dolor sit amet.</p>
             <p>
@@ -58,9 +46,8 @@ const FaqSection = () => {
               Necessitatibus, neque.
             </p>
           </div>
-          <div className="faq-line"></div>
-        </div>
-      </Toggle>
+        </Toggle>
+      </AnimateSharedLayout>
     </Faq>
   );
 };
